207b86d4e6b0156db4230e0b8b36c2feac87ce8b
Igor_U <fixtor75@gmail.com>
2022-10-01 23:07:16 +0300

    Задание2 Готово

---
+++ uCreate_user_list.py
@@ -0,0 +1,23 @@
+# Модуль для создания списков(изменяемые), кортежей(не изменяемы), словарей(изменяемые по ключу)
+
+
+def Int_list_create():
+    int_list = list(map(int,input('введите целые числа через пробел ').split()))
+    return int_list
+
+
+def Float_list_create():
+    float_list = list(map(float,input('введите вещественные числа через пробел ').split()))
+    return float_list
+
+
+# def Int_tupple_create():
+#     ....
+
+
+# def Float_tupple_create():
+#     ....
+
+
+# def Any_Dict():
+#     ....
