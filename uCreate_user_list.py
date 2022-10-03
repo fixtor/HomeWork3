bb782c4e0cb9da5c6688fc53e33a9f2a1551da0d
Igor_U <fixtor75@gmail.com>
2022-10-03 11:56:13 +0300

    Модуль для создания массива. Import в сценарий

--- uCreate_user_list.py
+++ uCreate_user_list.py
@@ -9,15 +9,3 @@ def Int_list_create():
 def Float_list_create():
     float_list = list(map(float,input('введите вещественные числа через пробел ').split()))
     return float_list
-
-
-# def Int_tupple_create():
-#     ....
-
-
-# def Float_tupple_create():
-#     ....
-
-
-# def Any_Dict():
-#     ....
